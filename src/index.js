097e540bad052c62c6221dc6935b76fafb28152b
ZeroneV1 <164795172+ZeroneV1@users.noreply.github.com>
2025-03-12 09:00:30 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,178 +1,42 @@
-// Imports
-var Logger = require('./modules/Logger');
-var Commands = require('./modules/CommandList');
-var GameServer = require('./GameServer');
-var figlet = require('figlet');
-var express = require('express'); // Added Express for Render
+const express = require("express");
+const WebSocket = require("ws");
 
-// Initialize Express (for Render Hosting)
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000; // Render assigns this dynamically
 
-app.get('/', (req, res) => {
-    res.send('MK Agario Server is running!');
+// Serve static files (client)
+app.use(express.static("web"));
+
+// Start Express server (Only listens on the assigned port)
+const server = app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
 
-// Start Express server (only for Render)
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// Attach WebSocket server to the same Express server
+const wss = new WebSocket.Server({ server });
 
-// Init variables
-var showConsole = process.env.RENDER ? false : true; // Disable console on Render
+wss.on("connection", (ws) => {
+    console.log("A client connected!");
+    ws.send("Welcome to the WebSocket server!");
 
-// Start msg
-setLoggerColorscheme();
-Logger.start();
-
-process.on('exit', function (code) {
-    Logger.debug("process.exit(" + code + ")");
-    Logger.shutdown();
-});
-
-process.on('uncaughtException', function (err) {
-    Logger.fatal(err.stack);
-    process.exit(1);
-});
-
-// Run MultiOgar-Edited
-var gameServer = new GameServer();
-Logger.info("\u001B[1m\u001B[32mMultiOgar-Edited " + gameServer.version + "\u001B[37m - An open source multi-protocol ogar server\u001B[0m");
-
-// Handle arguments
-process.argv.forEach(function (item) {
-    switch (item) {
-        case "--help":
-            console.log("Proper Usage: node index.js");
-            console.log("    -n, --name             Set name");
-            console.log("    -g, --gameport         Set game port");
-            console.log("    -s, --statsport        Set stats port");
-            console.log("    -m, --gamemode         Set game mode (id)");
-            console.log("    -c, --connections      Set max connections limit");
-            console.log("    -t, --tracker          Set serverTracker");
-            console.log("    -l, --light-background Set a light-background colorscheme for logger");
-            console.log("    --noconsole            Disables the console");
-            console.log("    --help                 Help menu");
-            console.log("");
-            break;
-
-        case "-n":
-        case "--name":
-            setParam("serverName", getValue(item));
-            break;
-
-        case "-g":
-        case "--gameport":
-            setParam("serverPort", parseInt(getValue(item)));
-            break;
-        case "-s":
-        case "--statsport":
-            setParam("serverStatsPort", parseInt(getValue(item)));
-            break;
-
-        case "-m":
-        case "--gamemode":
-            setParam("serverGamemode", getValue(item));
-            break;
-
-        case "-c":
-        case "--connections":
-            setParam("serverMaxConnections", parseInt(getValue(item)));
-            break;
-        case "-t":
-        case "--tracker":
-            setParam("serverTracker", parseInt(getValue(item)));
-            break;
-
-        case "-l":
-        case "--light-background":
-            // Has already been processed before logger initialization
-            break;
-
-        case "--noconsole":
-            showConsole = false;
-            break;
-    }
-});
-
-function setLoggerColorscheme() {
-    if (process.argv.includes("-l") || process.argv.includes("--light-background")) {
-        Logger.setLightBackgroundColorscheme();
-    }
-}
-
-function getValue(param) {
-    var ind = process.argv.indexOf(param);
-    var item = process.argv[ind + 1];
-    if (!item || item.startsWith('-')) {
-        Logger.error("No value for " + param);
-        return null;
-    } else {
-        return item;
-    }
-}
-
-function setParam(paramName, val) {
-    if (!gameServer.config.hasOwnProperty(paramName)) {
-        Logger.error("Wrong parameter");
-    }
-    if (val || val === 0) {
-        if (typeof val === 'string') {
-            val = "'" + val + "'";
-        }
-        eval("gameServer.config." + paramName + "=" + val);
-    }
-}
-
-gameServer.start();
-
-// ASCII Art Banner
-figlet(('MultiOgar-Edited  ' + gameServer.version), function (err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    console.log(data);
-});
-
-// Initialize the server console (Disabled on Render)
-if (showConsole) {
-    var readline = require('readline');
-    var in_ = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
+    ws.on("message", (message) => {
+        console.log(`Received: ${message}`);
     });
-    setTimeout(prompt, 100);
-}
 
-// Console functions (Disabled on Render)
-function prompt() {
-    if (!showConsole) return;
-    in_.question(">", function (str) {
-        try {
-            parseCommands(str);
-        } catch (err) {
-            Logger.error(err.stack);
-        } finally {
-            setTimeout(prompt, 0);
-        }
+    ws.on("close", () => {
+        console.log("Client disconnected");
     });
-}
+});
 
-function parseCommands(str) {
-    Logger.write(">" + str);
-    if (str === '') return;
+// Graceful shutdown handling
+process.on("SIGTERM", () => {
+    console.log("Shutting down server...");
+    server.close(() => {
+        console.log("HTTP Server closed.");
+    });
+    wss.close(() => {
+        console.log("WebSocket Server closed.");
+    });
+});
 
-    var split = str.split(" ");
-    var first = split[0].toLowerCase();
-    var execute = Commands.list[first];
-
-    if (typeof execute !== 'undefined') {
-        execute(gameServer, split);
-    } else {
-        Logger.warn("Invalid Command!");
-    }
-}
-
-exports.gameServer = gameServer;
+console.log("Server is set up and waiting for connections...");
